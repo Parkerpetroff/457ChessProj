03f2ed4cdcc6e6bb1a93bdcaba039eab04e44587
William <William@192.168.125.129>
2019-04-15 19:41:43 -0400

    server/client start

--- src/p2p/Client.java
+++ src/p2p/Client.java
@@ -2,155 +2,43 @@ package p2p;
 
 /**********************************************************************
  * Filename: Client
+
  *
  * Authors: Alec Betancourt, Parker Petroff, and Randy Nguyen
  **********************************************************************/
 
 import java.io.*;
-import java.net.Socket;
-import java.util.Scanner;
+import java.net.*;
 
 public class Client {
-
-    private Socket socket;
-    private String hostName;
-    private int portNumber;
-    private PrintWriter out;
-    private BufferedReader in;
-    private FileOutputStream fileOut;
-    private PrintWriter fileWrite;
-    private boolean exists;
-    private int status;
-    private DataInputStream din;
-    private String UserName;
-
-    public Client() {
-        // Something goes here?
-
-    }
-
-    public void setUserName(String s) {
-        UserName = s;
-    }
-
-    public int connect(String hostName, int portNumber) {
-        this.hostName = hostName;
-        this.portNumber = portNumber;
-        // Create a socket that connects to the above host and port number
-        try {
-            socket = new Socket(this.hostName, this.portNumber);
-            // Create a PrintWriter from the socket's output stream
-            // Use the autoFlush option
-            out = new PrintWriter(socket.getOutputStream(), true);
-            // Create a BufferedReader from the socket's input stream
-            //https://stackoverflow.com/questions/48266026/socket-java-client-python-server
-            din = new DataInputStream(socket.getInputStream());
-            out.println("CONNECT " + hostName + " " + portNumber + " " + UserName);
-            String s = din.readUTF();
-            System.out.println(s);
-        } catch (Exception e) {
-            System.out.println("error");
-            return -1;
+    public static void main(String[] args) throws IOException {
+        
+        if (args.length != 2) {
+            System.err.println("Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
         }
-        return 0;
-    }
 
-    public int list() {
-        try {
-            out.println("list");
-            String s = null;
-            System.out.println(s);
-        } catch (Exception e) {
-            System.out.println("No connection");
-            return -1;
-        }
-        return 0;
-    }
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
 
-    public int send (String com) {
-        try {
-            out.println(com);
-            System.out.println("Sent Command: " + com);
-            String s = din.readUTF();
-            while(din.readUTF()!=null) {
-                System.out.println(s);
+        try (
+            Socket echoSocket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
+        ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
             }
-        } catch (Exception e) {
-            System.out.println("Send Error");
-            return -1;
-        }
-        return 0;
-    }
-
-    public int close() {
-        try {
-            out.print("QUIT");
-            out.close();
-            socket.close();
-            return 0;
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
         } catch (IOException e) {
-            System.out.println("No connection to close");
-            return -1;
-        }
-    }
-
-    public static void main(String[] args) {
-        Client client = new Client();
-        Scanner scnr = new Scanner(System.in);
-
-        int status;
-
-        //System.out.print("Enter your username: ");
-        //String name = scnr.nextLine();
-        // TODO Remove fixed name
-        client.setUserName("RANDY");
-
-        while (true) {
-
-            String cmd = scnr.nextLine();
-            String[] commands = cmd.split(" ");
-
-            if (commands.length < 1 || commands.length > 3)
-                System.out.println("Invalid arguments");
-
-            else {
-
-                String command = commands[0].toUpperCase();
-                switch (command) {
-                    case "CONNECT":
-                        if (commands.length != 3)
-                            System.out.println("Invalid arguments");
-                        else {
-                            try {
-                                int pNum = Integer.parseInt(commands[2]);
-                                status = client.connect(commands[1], pNum);
-                                if (status == 0)
-                                    System.out.println("Connection Successful");
-                            } catch (Exception e) {
-                                System.out.print("Could not convert port to number");
-                            }
-                        }
-                        break;
-
-                    case "LIST":
-                        client.list();
-                        break;
-
-                    case "QUIT":
-                        status = client.close();
-                        break;
-
-                    case "HELP":
-                        System.out.println("--- HELP MENU ---\n");
-                        System.out.println("CONNECT <server name/IP address> <server port>");
-                        System.out.println("LIST");
-                        System.out.println("QUIT\n");
-                        break;
-
-                    default:
-                        client.send(command);
-                }
-            }
-        }
+            System.err.println("Couldn't get I/O for the connection to " +
+                hostName);
+            System.exit(1);
+        } 
     }
 }
--- src/p2p/Server.java
+++ src/p2p/Server.java
@@ -9,8 +9,7 @@ package p2p;
 import mainmenu.HostGUI;
 
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,173 +18,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+public class Server {
 
-public class Server implements Runnable {
-    private final boolean DEBUG = false;
-    private Path path;
-    private Socket controlSocket;
+    public static void main(String[] args) throws Exception {
 
+        // create socket
+        int port = 2222;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.err.println("Started server on port " + port);
 
-    public Server(Socket controlSocket) {
-
-        this.controlSocket = controlSocket;
-        path = Paths.get(System.getProperty("user.dir"));
-    }
-
-    @Override
-    public void run() {
-
-        //add some threading/port/socket debug messages
-        System.out.print("Thread id: " + Thread.currentThread().getId() + " of " + Thread.activeCount());
-        System.out.println(" " + controlSocket.getRemoteSocketAddress().toString().substring(1));
-
-        // Initialize Reader and Stream objects to null before running.
-        InputStreamReader iStream = null;
-        BufferedReader reader = null;
-        DataInputStream byteStream = null;
-        OutputStream oStream = null;
-        DataOutputStream dStream = null;
-
-        //add input/data/output streams and readers
-        //Input
-        try {
-            iStream = new InputStreamReader(controlSocket.getInputStream());
-            reader = new BufferedReader(iStream);
-        } catch (Exception e) {
-            System.out.println("Input Stream Error");
-        }
-
-        //Data
-        try {
-            byteStream = new DataInputStream(controlSocket.getInputStream());
-        } catch (Exception e) {
-            System.out.println("Byte Stream Error");
-        }
-
-        //Output
-        try {
-            oStream = controlSocket.getOutputStream();
-        } catch (Exception e) {
-            System.out.println("Output Stream Error");
-        }
-        try {
-            dStream = new DataOutputStream(oStream);
-        } catch (Exception e) {
-            System.out.println("Data Output Stream Error");
-        }
-        exitThread:
+        // repeatedly wait for connections, and process
         while (true) {
-            try {
-                //Server will try to connect every 20 ms
-                while (!reader.ready())
-                    Thread.sleep(10);
 
-                //capture and tokenize command
-                List<String> tokens = new ArrayList<String>();
-                String command = reader.readLine();
-                Scanner tokenize = new Scanner(command);
-                //gets command
-                if (tokenize.hasNext())
-                    tokens.add(tokenize.next());
-                //gets rest of string after the command; this allows filenames with spaces: 'file1 test.txt'
-                if (tokenize.hasNext())
-                    tokens.add(command.substring(tokens.get(0).length()).trim());
-                tokenize.close();
+            // a "blocking" call which waits until a connection is requested
+            Socket clientSocket = serverSocket.accept();
+            System.err.println("Accepted connection from client");
 
-                // switch statement for commands
-                switch (tokens.get(0)) {
-                    case "list":
-                        try {
-                            System.out.println("Listing");
-                            DirectoryStream<Path> dirStream = Files.newDirectoryStream(path);
-                            for (Path entry : dirStream)
-                                dStream.writeBytes(entry.getFileName() + "\n");
-                            dStream.writeBytes("\n");
-                        } catch (Exception e) {
-                            dStream.writeBytes("ls: failed to retrieve contents" + "\n");
-                            dStream.writeBytes("\n");
-                        }
-
-                        break;
-
-                    case "retrieve":
-                        //not a directory or file
-                        if (Files.notExists(path.resolve(tokens.get(1))))
-                            dStream.writeBytes("get: " + tokens.get(1) + ": No such file or directory" + "\n");
-
-                            //is a directory
-                        else if (Files.isDirectory(path.resolve(tokens.get(1))))
-                            dStream.writeBytes("get: " + tokens.get(1) + ": Is a directory" + "\n");
-
-                            //transfer file
-                        else {
-                            //blank message
-                            dStream.writeBytes("\n");
-
-                            File file = new File(path.resolve(tokens.get(1)).toString());
-                            long fileSize = file.length();
-
-                            //send file size
-                            dStream.writeBytes(fileSize + "\n");
-
-                            //need to figure
-                            Thread.sleep(100);
-
-                            byte[] buffer = new byte[8192];
-                            try {
-                                BufferedInputStream in = new BufferedInputStream(new FileInputStream(file));
-                                int count = 0;
-                                while ((count = in.read(buffer)) > 0)
-                                    dStream.write(buffer, 0, count);
-
-                                in.close();
-                                break;
-                            } catch (Exception e) {
-                                System.out.println("Error while retrieving");
-                            }
-                        }
-                        break;
-
-                    case "store":
-                        //get file size
-                        long fileSize = Long.parseLong(reader.readLine());
-                        FileOutputStream f = new FileOutputStream(new File(path.resolve(tokens.get(1)).toString()));
-                        int count = 0;
-                        byte[] buffer = new byte[8192];
-                        long bytesReceived = 0;
-                        while (bytesReceived < fileSize) {
-                            count = byteStream.read(buffer);
-                            f.write(buffer, 0, count);
-                            bytesReceived += count;
-                        }
-                        f.close();
-                        break;
-
-
-                    case "quit":
-                        if (DEBUG) System.out.println("-quit");
-                        //close socket
-                        controlSocket.close();
-
-                        //exit while loop
-                        break exitThread;
-                }
+            // open up IO streams
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        	BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        	
+            // waits for data and reads it in until connection dies
+            // readLine() blocks until the server receives a new line from client
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+            	System.out.println("Received message: " + inputLine + " from " + clientSocket.toString());
+        	    out.println(inputLine);
             }
-            catch (Exception e) {
-                System.out.println("Error while running");
-            }
-        }
-    }
 
-    //might have to be moved up to top function, check runnable use
-    public static void main(String[] args) throws IOException {
-        int port;
-        port = Integer.parseInt(args[0]);
-        System.out.println("Port Number: " + port);
-        ServerSocket socketControl = new ServerSocket(port);
-        while (true) {
-            //blocks until client connects; starts connection on new thread
-            new Thread(new Server(socketControl.accept())).start();
+            // close IO streams, then socket
+            System.err.println("Closing connection with client");
+            out.close();
+            in.close();
+            clientSocket.close();
         }
+        //when you quit
+        //serverSocket.close();
     }
 }
